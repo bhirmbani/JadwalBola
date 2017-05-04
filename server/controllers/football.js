40b33dc17180f2d291881f8ed68b7709bbaa4c78
Bhirawa Mbani <bhirmbani@gmail.com>
2017-05-04 19:52:18 +0700

    adding football api setup

---
+++ server/controllers/football.js
@@ -0,0 +1,9 @@
+const Football = require('../models/football');
+
+const methods = {};
+
+methods.get = (req, res, next) => {
+  
+}
+
+module.exports = methods;
---
+++ server/models/football.js
@@ -0,0 +1,4 @@
+var client= require('football-api-nodejs-client')('d50e127ef1404fd9a4a51c807899d109');
+
+client.getCompetitions()
+
