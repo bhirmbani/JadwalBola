8e9b19f0232be0a215657242303d8f811929f7b3
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2017-05-05 11:13:53 +0700

    add register

--- server/routes/footballs.js
+++ server/routes/footballs.js
@@ -3,6 +3,6 @@ var router = express.Router()
 var footballController = require('../controllers/footballController')
 const helper = require('../helpers/verify.js');
 
-router.get('/', helper.isUser, footballController.getAllUser);
+router.get('/', footballController.getAllUser);
 
 module.exports = router;
